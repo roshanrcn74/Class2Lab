8f2f7acb48604b77be041b12d2c601c02cd53216
roshann <roshann@10.30.23.58>
2017-09-04 11:47:19 -0500

    Class2Lab - #2

---
+++ web/js/javas.js
@@ -0,0 +1,32 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+var min = 0;
+var max = 3;
+var itemArray = ["Apple", "PineApple", "Mango", "Orange"];
+
+function filltable(){
+    var tbl=document.getElementById('tbl1');
+
+    var rowArray=tbl.rows;
+    //alert(rows.length);
+    for(var i=0;i<rowArray.length;i++){
+        var cellArray=rowArray[i].cells;
+        //alert(cells.length);
+        for(j=0;j<cellArray.length;j++){
+            //console.log(i+":"+j+"="+cellArray[j].innerHTML);
+            cellArray[j].innerHTML=itemArray[randomNumber()];
+        }
+    }
+}
+filltable();
+function randomNumber(){
+    rn = Math.floor(Math.random() * (max - min +1) + min);
+    console.log(rn);
+    return rn;    
+}
+
